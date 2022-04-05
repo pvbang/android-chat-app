62f3b96ad1e982c3985a2f769bddcb85857eaa97
Vĩnh Uy <vinhuy2002@gmail.com>
2022-04-05 12:17:56 +0700

    Post#1
    Add some post

--- app/src/main/java/com/example/chatapplication/fragments/storyfrag/StoryFragment.java
+++ app/src/main/java/com/example/chatapplication/fragments/storyfrag/StoryFragment.java
@@ -5,7 +5,10 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,14 +17,25 @@ import android.widget.Toast;
 
 import com.example.chatapplication.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class StoryFragment extends Fragment implements View.OnClickListener {
 
     private FloatingActionButton fab;
+    private ArrayList<StoryItem> storyItems = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private StoryRecyclerView storyRecyclerView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
@@ -29,9 +43,16 @@ public class StoryFragment extends Fragment implements View.OnClickListener {
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_story, container, false);
-
-        fab = (FloatingActionButton) view.findViewById(R.id.fab);
+        fab = view.findViewById(R.id.fab);
         fab.setOnClickListener(this::onClick);
+        recyclerView = view.findViewById(R.id.storyRecyclerView);
+        displayAllPosts();
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+        storyRecyclerView = new StoryRecyclerView(storyItems);
+        recyclerView.setAdapter(storyRecyclerView);
+        storyRecyclerView.notifyDataSetChanged();
+
         return view;
     }
 
@@ -43,5 +64,24 @@ public class StoryFragment extends Fragment implements View.OnClickListener {
                 startActivity(intent);
                 break;
         }
+    }
+
+    public void displayAllPosts(){
+        DatabaseReference reference = FirebaseDatabase.getInstance("https://chat-application-f4d3d-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference().child("Posts");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    StoryItem storyItem = dataSnapshot.getValue(StoryItem.class);
+                    storyItems.add(storyItem);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/chatapplication/fragments/storyfrag/StoryPost.java
+++ app/src/main/java/com/example/chatapplication/fragments/storyfrag/StoryPost.java
@@ -1,5 +1,6 @@
 package com.example.chatapplication.fragments.storyfrag;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
@@ -17,6 +18,20 @@ import com.example.chatapplication.R;
 import com.example.chatapplication.databinding.ActivityStoryPostBinding;
 import com.example.chatapplication.utilities.Constants;
 import com.example.chatapplication.utilities.PreferenceManager;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.EventListener;
+import java.util.HashMap;
 
 public class StoryPost extends AppCompatActivity implements View.OnClickListener {
 
@@ -24,6 +39,10 @@ public class StoryPost extends AppCompatActivity implements View.OnClickListener
     private TextView thinking, post, username;
     private EditText writeYourThinking;
     private PreferenceManager preferenceManager;
+    private String postDate, postTimeRan, postTimeOff, postRandomName, uid;
+
+    private DatabaseReference userRef, postRef;
+    private FirebaseFirestore firebaseFirestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,7 +67,13 @@ public class StoryPost extends AppCompatActivity implements View.OnClickListener
         post.setOnClickListener(this::onClick);
         imageBackStory.setOnClickListener(this);
 
+
+
+        userRef = FirebaseDatabase.getInstance("https://chat-application-f4d3d-default-rtdb.asia-southeast1.firebasedatabase.app").getReference().child("Users");
+        postRef = FirebaseDatabase.getInstance("https://chat-application-f4d3d-default-rtdb.asia-southeast1.firebasedatabase.app").getReference().child("Posts");
+
         loadInfo();
+        storeToFirebase();
 
     }
 
@@ -56,7 +81,9 @@ public class StoryPost extends AppCompatActivity implements View.OnClickListener
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.post:
-                Toast.makeText(this, "Đăng thành công", Toast.LENGTH_SHORT).show();
+                if (!writeYourThinking.getText().toString().isEmpty()) {
+                    savePost();
+                }
                 break;
             case R.id.imageBackStory:
                 onBackPressed();
@@ -69,5 +96,58 @@ public class StoryPost extends AppCompatActivity implements View.OnClickListener
         byte[] bytes = Base64.decode(preferenceManager.getString(Constants.KEY_IMAGE), Base64.DEFAULT);
         Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
         imageUser.setImageBitmap(bitmap);
+    }
+
+    public void storeToFirebase(){
+        Calendar calendarDate = Calendar.getInstance();
+        SimpleDateFormat currentDateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        postDate = currentDateFormat.format(calendarDate.getTime());
+
+        Calendar calendarTime = Calendar.getInstance();
+        SimpleDateFormat currentTimeFormat = new SimpleDateFormat("HH:mm:ss");
+        postTimeRan = currentTimeFormat.format(calendarTime.getTime());
+
+        postRandomName = postDate + postTimeRan;
+
+        Calendar calendarTimeOff = Calendar.getInstance();
+        SimpleDateFormat currentTimeFormatOff = new SimpleDateFormat("HH:mm");
+        postTimeOff = currentTimeFormatOff.format(calendarTimeOff.getTime());
+
+    }
+
+    public void savePost(){
+        String uidSave = preferenceManager.getString(Constants.KEY_USER_ID);
+        userRef.child(uidSave).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                    String userName = preferenceManager.getString(Constants.KEY_NAME);
+                    String imageProfile = preferenceManager.getString(Constants.KEY_IMAGE);
+
+                    HashMap postsMap = new HashMap();
+                    postsMap.put("uid", uidSave);
+                    postsMap.put("date", postDate);
+                    postsMap.put("time", postTimeOff);
+                    postsMap.put("description", writeYourThinking.getText().toString());
+                    postsMap.put("username", userName);
+                    postsMap.put("profileimage", imageProfile);
+
+                    postRef.child(uidSave+postRandomName).updateChildren(postsMap).addOnCompleteListener(new OnCompleteListener() {
+                        @Override
+                        public void onComplete(@NonNull Task task) {
+                            if (task.isSuccessful()){
+                                Toast.makeText(getApplicationContext(), "Đăng thành công", Toast.LENGTH_SHORT).show();
+                                onBackPressed();
+                            }
+                        }
+                    });
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
